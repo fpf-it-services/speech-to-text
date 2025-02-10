b93c42ac88568b513d472121e7504c197e185d33
fadelsew02 <sewadefadel@gmail.com>
2025-02-10 19:23:20 +0100

    mon commit

--- transcriber/urls.py
+++ transcriber/urls.py
@@ -3,6 +3,5 @@ from . import views
 
 urlpatterns = [
     path('api/transcribe/', views.transcribe_audio, name='transcribe_audio'),
-    path('api/translate/<str:target_language>/', views.translate_to_local_language, name='translate')
-    # Exemple d'URL : /translate/goun/
+    path('api/translate/<target_language>/', views.translate_to_local_language, name='translate')
 ]
--- transcriber/views.py
+++ transcriber/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 import assemblyai as aai
@@ -8,46 +7,35 @@ import os
 from django.http import JsonResponse
 from google import genai
 
+import logging
 
-# Configurez l'API Key d'AssemblyAI
+logger = logging.getLogger(__name__)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 aai.settings.api_key = "a6d31e8ed3c141529da0e65781508992"
 config = aai.TranscriptionConfig(language_code="fr")
 
 @api_view(['POST'])
 def transcribe_audio(request):
-    # Récupère le fichier audio envoyé dans le corps de la requête
     audio_file = request.FILES.get('audio')
 
     if not audio_file:
         return JsonResponse({'error': 'Aucun fichier audio envoyé.'}, status=400)
 
-    # Enregistre l'audio dans un dossier temporaire
     audio_path = os.path.join('media', 'audio.wav')
     
     with open(audio_path, 'wb') as f:
         for chunk in audio_file.chunks():
             f.write(chunk)
 
-    # Utilise AssemblyAI pour la transcription
     transcriber = aai.Transcriber(config=config)
     transcript = transcriber.transcribe(audio_path)
 
     if transcript.status == aai.TranscriptStatus.error:
-        os.remove(audio_path)  # Supprime le fichier si la transcription échoue
+        os.remove(audio_path)  
         return JsonResponse({'error': f'Transcription échouée : {transcript.error}'}, status=500)
 
-    # Supprime le fichier audio après transcription
     os.remove(audio_path)
-    print(transcript.text)
-    
-    
-
-    # client = genai.Client(api_key="AIzaSyB9AhT3ROhLlPrmTbKiChwUQZ913SiDfoU")
-    # response = client.models.generate_content(
-    #     model="gemini-2.0-flash", contents=f'Traduit en langue {target_language} ceci sans markdown: {transcript.text}'
-    # )
-    # print(response.text)
-
 
     data = {
         "message": transcript.text,
@@ -57,9 +45,12 @@ def transcribe_audio(request):
 
 @api_view(['POST'])
 def translate_to_local_language(request, target_language):
+    logger.info(f"Route visitée : {request.build_absolute_uri()} - Traduction demandée en {target_language}")
     text = request.data.get('text')
+    logger.info(f"Texte reçu pour traduction : {text}")
 
     if not text:
+        logger.error("Aucun texte à traduire envoyé.")
         return JsonResponse({'error': 'Aucun texte à traduire envoyé.'}, status=400)
 
     try:
